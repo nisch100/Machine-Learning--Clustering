931449410fdd60fce5ffded9f73897a6ae6e22e6
Nischal Reddy <chandra.nischal@gmail.com>
2017-06-29 03:52:56 -0400

    Update kmeans.py

--- Clustering/K-Means Clustering/kmeans.py
+++ Clustering/K-Means Clustering/kmeans.py
@@ -1,16 +1,14 @@
 # K-Means Clustering
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
 # y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
+# Training set and Test set
 """from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
@@ -22,7 +20,7 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Using the elbow method to find the optimal number of clusters
+# Elbow Method
 from sklearn.cluster import KMeans
 wcss = []
 for i in range(1, 11):
@@ -39,7 +37,7 @@ plt.show()
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 42)
 y_kmeans = kmeans.fit_predict(X)
 
-# Visualising the clusters
+# Visualisation
 plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
 plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Cluster 2')
 plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Cluster 3')
